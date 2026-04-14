accd4fd70a714a732aae227becb5e01f00a8cc12
FATIMA ZAHRA <1728829@alu.murciaeduca.es>
2026-04-14 23:43:12 +0200

    Controladores hechos pero estan pendientes de correciones

---
+++ app/Http/Controllers/Docente/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Docente;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+     public function __invoke(): View
+    {
+        $docenteRoleId = Role::where('name', 'docente')->value('id');
+
+        $ecosistemas = auth()->user()
+            ->userRoles()
+            ->where('role_id', $docenteRoleId)
+            ->with([
+                'ecosistemaLaboral.modulo',
+                'ecosistemaLaboral.situacionesCompetencia',
+                'ecosistemaLaboral.perfilesHabilitacion',
+            ])
+            ->get()
+            ->pluck('ecosistemaLaboral')
+            ->filter();
+
+        return view('docente.dashboard', compact('ecosistemas'));
+    }
+}
